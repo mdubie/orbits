77cca34b337b3367676e186b96ebfa34eac0bd2d
Matt Dubie <mbdubie@gmail.com>
2017-08-31 01:03:03 -0700

    added ship

--- resources/public/out/game/constants.cljc
+++
@@ -1,3 +0,0 @@
-(ns game.constants)
-
-(def colors ["0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "A" "B" "C" "D" "E"])
--- resources/public/out/game/constants.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.7.122 {}
-goog.provide('game.constants');
-goog.require('cljs.core');
-game.constants.colors = new cljs.core.PersistentVector(null, 15, 5, cljs.core.PersistentVector.EMPTY_NODE, ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E"], null);
-
-//# sourceMappingURL=constants.js.map
